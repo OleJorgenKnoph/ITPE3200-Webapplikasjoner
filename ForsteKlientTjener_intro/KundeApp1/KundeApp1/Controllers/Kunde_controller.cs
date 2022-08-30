d12e227e6ca21003c517a7bccc61c75b6b7a6785
Ole Jørgen Knoph <s354371@oslomet.no>
2022-08-30 14:16:08 +0200

    Fulgte første tutorial. Localhost 404 not found. Prøve på nytt senere

---
+++ ForsteKlientTjener_intro/KundeApp1/KundeApp1/Controllers/Kunde_controller.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using KundeApp1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KundeApp1.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class Kunde_controller : ControllerBase
+    {
+
+        public List<Kunde> HentAlle()
+        {
+            var kundene = new List<Kunde>();
+
+            var kunde1 = new Kunde();
+            kunde1.navn = "Ole Hansen";
+            kunde1.adresse = "Haneborgveien 32";
+
+            var kunde2 = new Kunde
+            {
+                navn = "Line Hansen",
+                adresse = "Osloveien 69"
+            };
+
+            kundene.Add(kunde1);
+            kundene.Add(kunde2);
+
+            return kundene;
+        }
+    }
+}
+
---
+++ ForsteKlientTjener_intro/KundeApp1/KundeApp1/Models/Kunde.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace KundeApp1.Models
+{
+    public class Kunde
+    {
+        public string navn { get; set; }
+        public string adresse { get; set; }
+
+    }
+}
+
